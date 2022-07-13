45977e992cdef675941142f9b7645b08214997e1
urmil404 <urmil8989@gmail.com>
2022-07-13 16:45:28 +0530

    theme-swither

---
+++ src/components/HeroSection.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import ThemeContext from "../Context/ThemeContext";
+import AppTheme from "../Colors";
+
+const HeroSection = () => {
+  const theme = useContext(ThemeContext)[0];
+  const currentTheme = AppTheme[theme];
+
+  return (
+    <div
+      style={{
+        padding: "1rem",
+        backgroundColor: `${currentTheme.backgroundColor}`,
+        color: `${currentTheme.textColor}`,
+      }}
+    >
+      <h1>Context API theme toggler</h1>
+      <button style={{ backgroundColor: "#26ae60", padding: "10px 150px" }}>
+        Click Me
+      </button>
+    </div>
+  );
+};
+
+export default HeroSection;
